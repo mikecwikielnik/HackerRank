305b4a0ea78ace0630d661321d89d23fd98bd007
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-10-25 21:01:46 -0400

    Add files via upload

---
+++ Problem Solving/Algorithms/warm ups/simpleArraySum.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    sum = 0
+
+    for i in ar:
+        sum = sum + i
+    
+    return(sum)
+        
+if __name__ == '__main__':
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    print(result)
